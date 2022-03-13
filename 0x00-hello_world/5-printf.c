e53455aa67472f6693aa1615add39230e6de4fd0
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-03-13 04:06:22 -0700

    my secound program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *my heloo, printf task
+ *return: return 1 after program execuition 
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
