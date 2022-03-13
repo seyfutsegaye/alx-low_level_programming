a10441934d6bd952d95651f6de8fcd25445defa7
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-03-13 03:36:58 -0700

    my first program heloo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+  *main - entery point
+  *
+  * reeturn 0 after printing the function
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
