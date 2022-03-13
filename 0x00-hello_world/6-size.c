ae1cc3940630f410cb542604c891e116d47e0354
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-03-13 05:30:14 -0700

    my last  c proram  file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
-  * 
-  * main entery - point
+  *
+  *main entery - point
   *
   *Return: Alwayes 0 (Sucess)
   */
 int main(void)
 {
-	prntf("size of char: %d byte(s)\n", sizeof(char));
-	prntf("size of int: %d byte(S)\n", sizeof(int));
-	prntf("size of long int: %d byte(S)\n", sizeof(long int));
-	prntf("size of long long int: %d byte(S)\n", sizeof(long long int));
-	prntf("size of float: %d byte(S)\n", sizeof(float));
-	return(0);
+prntf("size of char: %d byte(s)\n", sizeof(char));
+prntf("size of int: %d byte(S)\n", sizeof(int));
+prntf("size of long int: %d byte(S)\n", sizeof(long int));
+prntf("size of long long int: %d byte(S)\n", sizeof(long long int));
+prntf("size of float: %d byte(S)\n", sizeof(float));
+return(0);
 }
