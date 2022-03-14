e8fc0165bee9b81ab20638ae06e0df07b75698ea
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-03-14 11:47:07 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,14 +12,18 @@ char lower_case;
 
 for (upper_case = 'A'; upper_case <=  'Z'; upper_case++)
 {
-putchar(upper_case)
+putchar(upper_case);
 }
 
-for (lower_case = 'a'; lower_case <= 'z'; lower_case++
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
-putchar(lower_case)
+putchar(lower_case);
 }
+
+putchar ('\n');
+
 return (0);
+
 }
 
 
