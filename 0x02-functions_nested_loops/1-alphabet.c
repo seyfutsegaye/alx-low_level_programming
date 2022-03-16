8225a4386d8dc98a1bc5fb25d0f359b8dd281893
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-03-16 11:57:56 -0700

    my commit message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabet lowercase
+ *print_alphabet - print alphabet lowercase
  *
- * Return: Always 0.
+ *Return: Always 0
  */
-int main(void)
 
 void print_alphabet(void)
 {
