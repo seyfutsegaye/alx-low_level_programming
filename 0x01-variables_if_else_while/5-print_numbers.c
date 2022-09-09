b83ccea00d557d5c559eb7c2b072c69b0db571e5
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-09-09 12:05:43 -0700

    now lets print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a random number and state wheather it is
+ * postive negative or zero
+ * Return: Alwayes 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
