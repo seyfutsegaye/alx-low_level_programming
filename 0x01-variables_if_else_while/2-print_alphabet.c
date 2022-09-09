ba74502400bea9fdd657408416e862356c39a896
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-09-09 10:56:11 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - prints a random number and state wheather it is
+ *  postive negative or zero
+ *  Return: Alwayes 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+putchar('n');
+return (0);
+}
