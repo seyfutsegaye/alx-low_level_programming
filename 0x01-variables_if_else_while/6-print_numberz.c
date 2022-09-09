390dffe29ddb848f7f75ac8dcf968f316bea20c5
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-09-09 12:32:41 -0700

    now lets reverse the letters

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints a random number and state wheather it is
+ * postive negative or zero
+ * Return: Alwayes 0
+ */
+int main(void)
+{
+
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a random number and state wheather it is
+ * postive negative or zero
+ * Return: Alwayes 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
