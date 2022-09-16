6ab242092fcc7ba5d1ad4f820fdfdb6d6c22b031
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-09-16 04:22:52 -0700

    print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to display numers from 0 to 9
+ * 0 to 9
+ *
+ * Returns: return nothing
+ */
+void print_numbers(void)
+{
+int n;
+
+for (n = 48; n <= 58; n++)
+{
+	_putchar(n);
+}
+_putchar(10);
+}
