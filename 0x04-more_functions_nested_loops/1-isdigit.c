c182d33aab35b08f3779b94c54d4a0b272795ca8
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-09-16 01:10:49 -0700

    lets cheak if number is digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - function to cheak for digits
+ * @c: is the  int used for argument
+ * Return: Always 0
+ */
+int _isdigit(int c)
+{
+for (c = 0; c <= 9; c++)
+
+if (c == 0)
+{
+return (0);
+}
+else
+return (1);
+}
+
