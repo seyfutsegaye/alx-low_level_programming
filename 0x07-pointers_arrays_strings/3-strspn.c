ad9340c58854076a6d0047db01c58025d48ef94c
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-09-26 01:25:24 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - print the conscuative character of
+ * @s: source string
+ * @accept: searching string
+ *
+ * Return: new string
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+
+for (j = 0; *(s + j); j++)
+{
+for (i = 0; *(accept + i); i++)
+{
+if (*(s + j) == *(accept + i))
+break;
+}
+if (*(accept + i) == '\0')
+break;
+}
+return (1);
+}
