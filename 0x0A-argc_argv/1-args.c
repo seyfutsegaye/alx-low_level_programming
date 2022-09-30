ff039680518e368bc90fbb213086097f0a32a954
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-09-30 10:22:06 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of a program
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: Alwayes 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%i\n", argc - 1);
+return (0);
+}
+
