cad8ea3571e4eeb1297b4ba72d5989f313b6a80a
seyfutsegaye <seyfutsegaye1995@gmail.com>
2022-10-13 10:31:00 -0700

    tas1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - print the number followed by a new line
+ * @separator - string to printed
+ * @n: number of intigers
+ *
+ * Return :void;
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list numbers;
+
+va_start(numbers, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(numbers, int));
+if (i <  n - 1 && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(numbers);
+}
